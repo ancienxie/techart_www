4498a6d07ab5a196ebd83ffab9c167b06781b403
ancienxie <Anton@mail.com>
2024-07-08 14:58:55 +0300

    sixth commit - monday - date

--- www/harder_version/news.php
+++ www/harder_version/news.php
@@ -5,7 +5,7 @@
 
 	$id = $_GET['id'] ?? 0;
 
-    $sql = "SELECT *, DATE_FORMAT('date','%d.%m.%Y') news_date FROM news WHERE id=?";
+    $sql = "SELECT *, DATE_FORMAT(`date`,'%d.%m.%Y') news_date FROM news WHERE id=?";
 
     $res = $db->prepare($sql);
 
